bfb087d6e92eb7aca85f6bb2dedc29b53d3c825d
nunezag816 <nunezag@msoe.edu>
2025-06-29 21:04:48 -0500

    Create GameLogTest.java

---
+++ app/src/test/java/org/example/GameLogTest.java
@@ -0,0 +1,19 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameLogTest {
+
+    @Test
+    public void testWinCounts() {
+        GameLog log = new GameLog();
+        log.recordWin('X');
+        log.recordWin('O');
+        log.recordWin('-');
+
+        assertEquals(1, log.getXWins());
+        assertEquals(1, log.getOWins());
+        assertEquals(1, log.getTies());
+    }
+}
