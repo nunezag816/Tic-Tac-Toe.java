838b05004f7bb1a57678244c5290f5bb1e7e355c
nunezag816 <nunezag@msoe.edu>
2025-07-13 19:55:02 -0500

    Update Main.java

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         TicTacToeGame game = new TicTacToeGame();
-        game.run();
+        Scanner scanner = new Scanner(System.in);
+        game.run(scanner);
+        scanner.close();
     }
 }
