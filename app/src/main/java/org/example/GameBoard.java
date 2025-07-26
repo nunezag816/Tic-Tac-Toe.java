a6518d0dc43eb49022d72c678c1977e9c44b9c84
nunezag816 <nunezag@msoe.edu>
2025-07-25 21:01:17 -0500

    Update GameBoard.java

--- app/src/main/java/org/example/GameBoard.java
+++ app/src/main/java/org/example/GameBoard.java
@@ -1,58 +1,90 @@
 package org.example;
 
-package model;
-
-import java.util.Arrays;
-
 public class GameBoard {
-    private char[] board = new char[9];
+    private final char[] board;
 
-    public Board() {
-        Arrays.fill(board, ' ');
+    public GameBoard() {
+        board = new char[9];
+        resetBoard();
     }
 
-    public boolean isFull() {
-        for (char c : board) if (c == ' ') return false;
-        return true;
+    public void resetBoard() {
+        for (int i = 0; i < 9; i++) {
+            board[i] = ' ';
+        }
     }
 
-    public boolean makeMove(int position, char symbol) {
-        if (position < 1 || position > 9 || board[position - 1] != ' ') return false;
-        board[position - 1] = symbol;
-        return true;
+    public boolean isMoveValid(int position) {
+        return position >= 1 && position <= 9 && board[position - 1] == ' ';
+    }
+
+    public void makeMove(int position, char player) {
+        if (isMoveValid(position)) {
+            board[position - 1] = player;
+        }
+    }
+
+    public void makeTemporaryMove(int position, char player) {
+        board[position - 1] = player;
+    }
+
+    public void undoTemporaryMove(int position) {
+        board[position - 1] = ' ';
     }
 
     public boolean isEmpty() {
-        for (char c : board) if (c != ' ') return false;
+        for (char c : board) {
+            if (c != ' ') {
+                return false;
+            }
+        }
         return true;
     }
 
+    public int getMoveCount() {
+        int count = 0;
+        for (char c : board) {
+            if (c != ' ') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public char[] getBoardCopy() {
+        return board.clone();
+    }
+
     public void display() {
+        System.out.println();
         for (int i = 0; i < 9; i++) {
-            System.out.printf("  %c  ", board[i] == ' ' ? (char) ('1' + i) : board[i]);
-            if ((i + 1) % 3 == 0 && i != 8) System.out.println("\n-----+-----+-----");
-            else if ((i + 1) % 3 != 0) System.out.print("|");
+            char cell = (board[i] == ' ') ? (char) ('1' + i) : board[i];
+            System.out.print(" " + cell + " ");
+            if (i % 3 != 2) {
+                System.out.print("|");
+            } else if (i != 8) {
+                System.out.println("\n-----------");
+            }
         }
         System.out.println();
     }
 
     public char checkWinner() {
-        int[][] lines = {
-            {0,1,2}, {3,4,5}, {6,7,8},
-            {0,3,6}, {1,4,7}, {2,5,8},
-            {0,4,8}, {2,4,6}
+        int[][] winPatterns = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, 
+            {0, 4, 8}, {2, 4, 6}             
         };
-        for (int[] line : lines) {
-            if (board[line[0]] != ' ' &&
-                board[line[0]] == board[line[1]] &&
-                board[line[1]] == board[line[2]]) {
-                return board[line[0]];
+
+        for (int[] pattern : winPatterns) {
+            char a = board[pattern[0]];
+            char b = board[pattern[1]];
+            char c = board[pattern[2]];
+            if (a != ' ' && a == b && b == c) {
+                return a;
             }
         }
-        return ' ';
-    }
 
-    public char[] getBoard() {
-        return board.clone();
+        return (getMoveCount() == 9) ? '-' : '-';
     }
 }
